c142acac0155dcda97429055e445e11983224799
codyrush <codyrush@gmail.com>
2019-04-29 19:39:39 -0700

    push

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="custom_nav navbar navbar-expand-lg navbar-light bg-light">
                 <h1 className="navbar-brand"><a href="/">Google Books</a></h1>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
